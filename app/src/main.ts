861a91a1c3e69f8c219aef3764930e208e57ebc7
mest2011 <mest2011mest@gmail.com>
2025-07-09 00:53:10 -0300

    chore: adjusted file names and muted warning messages of scss

--- app/src/main.ts
+++ app/src/main.ts
@@ -3,7 +3,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
-import './assets/custom-bootstrap.scss'
+import './assets/styles.scss'
 
 const app = createApp(App)
 
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,5 +3,10 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: { quietDeps: true },
+    },
+  },
   plugins: [vue()],
 });
